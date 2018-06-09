e343fe3c5d6d89c4b7bbc178e048fad32d19dd6b
Kareem Hepburn <kareemgan@gmail.com>
2018-06-09 11:43:31 -0500

    add http client interface

---
+++ http_client.go
@@ -0,0 +1,16 @@
+package paratils
+
+import (
+	"io"
+	"net/http"
+	"net/url"
+)
+
+// HTTPClient ...
+type HTTPClient interface {
+	Do(*http.Request) (*http.Response, error)
+	Get(string) (*http.Response, error)
+	Head(string) (*http.Response, error)
+	Post(string, string, io.Reader) (*http.Response, error)
+	PostForm(string, url.Values) (*http.Response, error)
+}
