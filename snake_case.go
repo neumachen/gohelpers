e33e4ba656defaecc74e9642a0785615b6e7d978
Kareem Hepburn <kareemgan@gmail.com>
2018-12-31 17:58:07 -0600

    Add snake case

---
+++ snake_case.go
@@ -0,0 +1,34 @@
+package paratils
+
+import (
+	"regexp"
+	"strings"
+)
+
+var numberSequence = regexp.MustCompile(`([a-zA-Z])(\d+)([a-zA-Z]?)`)
+var numberReplacement = []byte(`$1 $2 $3`)
+
+func addWordBoundariesToNumbers(s string) string {
+	b := []byte(s)
+	b = numberSequence.ReplaceAll(b, numberReplacement)
+	return string(b)
+}
+
+// SnakeCase transforms the s into snake cased format
+func SnakeCase(s string) string {
+	s = addWordBoundariesToNumbers(s)
+	s = strings.Trim(s, " ")
+	key := ""
+	for i, v := range s {
+		if i > 0 && v >= 'A' && v <= 'Z' && key[len(key)-1] != '_' {
+			key += "_" + string(v)
+		} else if v == ' ' {
+			key += "_"
+		} else if v == '/' || v == '\\' {
+			key += "_or_"
+		} else {
+			key = key + string(v)
+		}
+	}
+	return strings.ToLower(key)
+}
---
+++ snake_case_test.go
@@ -0,0 +1,31 @@
+package paratils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSnakeCase(t *testing.T) {
+	cc := [][]string{
+		[]string{"testCase", "test_case"},
+		[]string{"TestCase", "test_case"},
+		[]string{"Test Case", "test_case"},
+		[]string{" Test Case", "test_case"},
+		[]string{"Test Case ", "test_case"},
+		[]string{" Test Case ", "test_case"},
+		[]string{"test", "test"},
+		[]string{"test_case", "test_case"},
+		[]string{"Test", "test"},
+		[]string{"", ""},
+		[]string{"ManyManyWords", "many_many_words"},
+		[]string{"manyManyWords", "many_many_words"},
+		[]string{"AnyKind of_string", "any_kind_of_string"},
+		[]string{"numbers2and55with000", "numbers_2_and_55_with_000"},
+		[]string{"Foo/Boo", "foo_or_boo"},
+		[]string{"Foo/Boo/Moo", "foo_or_boo_or_moo"},
+	}
+	for _, c := range cc {
+		assert.Equal(t, c[1], SnakeCase(c[0]))
+	}
+}
