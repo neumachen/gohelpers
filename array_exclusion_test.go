b439651fa8ade47a2c3e13865a49eec84e881c4a
Kareem H <kareemgan@gmail.com>
2021-03-20 17:52:16 -0500

    Fix flapping tests

--- array_exclusion_test.go
+++ array_exclusion_test.go
@@ -17,10 +17,12 @@ func TestSliceExclusionStrings(t *testing.T) {
 		"c",
 		"d",
 	}
+	expectedLeft := []string{"a"}
+	expectedRight := []string{"d"}
 
 	left, right := SliceExclusionStrings(a, b)
-	require.Equal(t, []string{"a"}, left)
-	require.Equal(t, []string{"d"}, right)
+	require.True(t, testEqSliceStrs(expectedLeft, left), expectedLeft, left)
+	require.True(t, testEqSliceStrs(expectedRight, right), expectedRight, right)
 }
 
 func TestSliceExclusionInts(t *testing.T) {
@@ -34,8 +36,10 @@ func TestSliceExclusionInts(t *testing.T) {
 		5,
 		6,
 	}
+	expectedLeft := []int{1, 2}
+	expectedRight := []int{5, 6}
 
 	left, right := SliceExclusionInts(a, b)
-	require.Equal(t, []int{1, 2}, left)
-	require.Equal(t, []int{5, 6}, right)
+	require.True(t, testEqSliceInts(expectedLeft, left), expectedLeft, left)
+	require.True(t, testEqSliceInts(expectedRight, right), expectedRight, right)
 }
--- array_intersect_test.go
+++ array_intersect_test.go
@@ -1,7 +1,6 @@
 package paratils
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -18,10 +17,11 @@ func TestSliceIntersectStrings(t *testing.T) {
 		"c",
 		"d",
 	}
+	expected := []string{"b", "c"}
 
 	result := SliceIntersectStrings(a, b)
 	require.True(t, len(result) == 2)
-	require.True(t, reflect.DeepEqual([]string{"b", "c"}, result))
+	require.True(t, testEqSliceStrs(expected, result), expected, result)
 }
 
 func TestSliceIntersectInts(t *testing.T) {
@@ -35,8 +35,9 @@ func TestSliceIntersectInts(t *testing.T) {
 		3,
 		4,
 	}
+	expected := []int{2, 3}
 
 	result := SliceIntersectInts(a, b)
 	require.True(t, len(result) == 2)
-	require.True(t, reflect.DeepEqual([]int{2, 3}, result))
+	require.True(t, testEqSliceInts(expected, result), expected, result)
 }
