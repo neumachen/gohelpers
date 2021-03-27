3e898ac7aca2bec3d5a6f59681764e77e7bde704
Kareem H <kareemgan@gmail.com>
2021-03-27 16:11:06 -0500

    Add helper method for initializing new Errx

--- errx.go
+++ errx.go
@@ -1,6 +1,10 @@
 package paratils
 
-import "github.com/ParaServices/errgo"
+import (
+	"errors"
+
+	"github.com/ParaServices/errgo"
+)
 
 // Errx wraps the current e into an errgo.Error if it's not of that type. If
 // if is, it ignores and returns it as is. If the e is nil, it returns nil
@@ -11,4 +15,15 @@ func Errx(e error) error {
 		return err
 	}
 	return errgo.New(e)
+}
+
+// NewErrx helper methods for initializing a new error.
+func NewErrx(s string) error {
+	var err error
+
+	if s != "" {
+		err = errors.New(s)
+	}
+
+	return errgo.New(err)
 }
