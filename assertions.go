325f889b3b19fd76ec977478a40e24fa9ea8b9b5
Kareem H <kareemgan@gmail.com>
2021-05-17 22:05:26 -0500

    Extend IsNil

--- assertions.go
+++ assertions.go
@@ -19,6 +19,16 @@ func IsNil(v interface{}) bool {
 	return v == nil || (reflect.ValueOf(v).Kind() == reflect.Ptr && reflect.ValueOf(v).IsNil())
 }
 
+// AreNil checks if the given interface is nil.
+func AreNil(v ...interface{}) bool {
+	for i := range v {
+		if !IsNil(v[i]) {
+			return false
+		}
+	}
+	return true
+}
+
 type StringGetter interface {
 	GetString() string
 }
