4d35f448aa54a48767a160c891c5df4466410ea4
Kareem H <kareemgan@gmail.com>
2021-06-11 22:58:50 -0500

    Update errgo and depracate Errx

--- errx.go
+++ errx.go
@@ -11,6 +11,7 @@ import (
 // Errx wraps the current e into an errgo.Error if it's not of that type. If
 // if is, it ignores and returns it as is. If the e is nil, it returns nil
 // (this is handled in the errgo package)
+// DEPRECATED: use errgo.New directly (latest version >=v0.5.0)
 func Errx(e error) error {
 	err, ok := e.(*errgo.Error)
 	if ok {
